a6429e0e348bac90428c4e0a261083fe1f36b734
Adina <cioconea.adina@gmail.com>
2024-07-30 02:04:07 -0700

    Start

---
+++ IBO/src/test/java/com/DB/IBO/IboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DB.IBO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
